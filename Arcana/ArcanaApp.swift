465a990bd3abd75ca7cdefbeea08bc63eb165a57
incidentallycilantro <derskine25@proton.me>
2025-07-22 20:46:56 -0400

    Initial Commit

---
+++ Arcana/ArcanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcanaApp.swift
+//  Arcana
+//
+//  Created by Dylan Erskine on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ArcanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
